364ebfc184f434bf4a6cf272b677e2f1eae422be
ahamed raziuddin <ahamedrazi3@gmail.com>
2025-12-21 21:01:10 -0500

    chore: add prometheus and grafana monitoring stack

--- services/gemini-adapter/src/main/java/com/llmcompare/gemini_adapter/controller/GeminiController.java
+++ services/gemini-adapter/src/main/java/com/llmcompare/gemini_adapter/controller/GeminiController.java
@@ -9,7 +9,6 @@ public class GeminiController {
     @PostMapping("/gemini")
     public Mono<String> callGemini(@RequestBody String prompt) {
 
-        return  Mono.error(new RuntimeException("Error processing request: " ));
-                //Mono.just("GEMINI response for prompt: " + prompt);
+        return  Mono.just("GEMINI response for prompt: " + prompt);
     }
 }
